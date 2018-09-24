0034ce1dbbe71975fc8956d33514e36a8fcd4c32
Soumya Das <soumya.das2@pfizer.com>
2018-09-24 23:57:57 +0530

    Updated docker file.

--- multi_projects/prj2/index.php
+++ multi_projects/prj2/index.php
@@ -3,7 +3,7 @@
     <h1>B PHP</h1>
 
     <?php echo "PHP Works!";    ?>
-    <?php $mysqli = new mysqli("drupal", "drupal", "drupal");
+    <?php $mysqli = new mysqli("mariadb", "drupal", "drupal", "drupal");
     echo $mysqli->server_info;
     ?>
   </body>
