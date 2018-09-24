0916d26b7876b87e89f9d14b4fab58aa71a837e2
Soumya Das <soumya.das2@pfizer.com>
2018-09-24 08:30:24 +0530

    Added multi project docker composer.

---
+++ multi_projects/prj1/index.php
@@ -0,0 +1,7 @@
+<html>
+  <body>
+    <h1>A PHP</h1>
+
+    <?php echo "PHP Works!";?>
+  </body>
+</html>
---
+++ multi_projects/prj2/index.php
@@ -0,0 +1,10 @@
+<html>
+  <body>
+    <h1>B PHP</h1>
+
+    <?php echo "PHP Works!";    ?>
+    <?php $mysqli = new mysqli("drupal", "drupal", "drupal");
+    echo $mysqli->server_info;
+    ?>
+  </body>
+</html>
